ebdbda1d49fac187c9db516ad84746e1ff99960a
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2019-08-13 16:26:34 +0800

    added homecreen with simple text and button

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import * as React from "react";
 import { mapping, light as lightTheme } from "@eva-design/eva";
 import { ApplicationProvider, Layout } from "react-native-ui-kitten";
+import { HomeScreen } from "./src/components/screens/HomeScreen";
 
 const App = () => (
   <ApplicationProvider mapping={mapping} theme={lightTheme}>
-    <Layout style={{ flex: 1 }} />
+    <HomeScreen />
   </ApplicationProvider>
 );
 
